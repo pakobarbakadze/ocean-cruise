7f6948cb413375e9059d6112a7d0b595ad4220b3
pakobarbakadze <pakobarbakadze@gmail.com>
2023-12-08 17:39:37 +0400

    feat: cabin

---
+++ cabin.ts
@@ -0,0 +1,52 @@
+import { CabinType, EntertainmentActivity } from "./interfaces";
+
+export class Cabin {
+  constructor(public type: CabinType, public numberOfCabins: number) {}
+
+  public calculateTotalCost(activities: EntertainmentActivity[]): number {
+    const baseCost = this.getBaseCost();
+    const additionalFees = this.calculateAdditionalFees(activities);
+    return baseCost + additionalFees;
+  }
+
+  protected getBaseCost(): number {
+    return 0;
+  }
+
+  private calculateAdditionalFees(activities: EntertainmentActivity[]): number {
+    return activities.reduce(
+      (total, activity) => total + (activity.price ?? 0),
+      0
+    );
+  }
+}
+
+export class EconomyCabin extends Cabin {
+  constructor(numberOfCabins: number) {
+    super(CabinType.Economy, numberOfCabins);
+  }
+
+  public getBaseCost(): number {
+    return 100;
+  }
+}
+
+export class BusinessCabin extends Cabin {
+  constructor(numberOfCabins: number) {
+    super(CabinType.Business, numberOfCabins);
+  }
+
+  public getBaseCost(): number {
+    return 200;
+  }
+}
+
+export class LuxuryCabin extends Cabin {
+  constructor(numberOfCabins: number) {
+    super(CabinType.Luxury, numberOfCabins);
+  }
+
+  public getBaseCost(): number {
+    return 500;
+  }
+}
---
+++ interfaces/index.ts
@@ -0,0 +1,33 @@
+export interface Location {
+  from: string;
+  to: String;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface Schedule {
+  locations: Location[];
+}
+
+export interface Task {
+  description: string;
+  status: string;
+}
+
+export enum CrewMemberType {
+  Captain = "Captain",
+  Mechanic = "Mechanic",
+  Cook = "Cook",
+  Cleaner = "Cleaner",
+}
+
+export enum CabinType {
+  Economy = "Economy",
+  Business = "Business",
+  Luxury = "Luxury",
+}
+
+export interface EntertainmentActivity {
+  name: string;
+  price?: number;
+}
